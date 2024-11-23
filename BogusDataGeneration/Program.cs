7d0f54860ad116f051db44761b558a63512fadfa
kapilguptag13125 <kapil.gupta.g@gmail.com>
2024-11-23 20:00:26 +0000

    added new files

---
+++ BogusDataGeneration/Program.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+
+namespace BogusDataGeneration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userFaker = new Faker<User>()
+            .RuleFor(u => u.Id, f => f.IndexFaker + 1) // Auto-increment Id
+            .RuleFor(u => u.FullName, r => $"{r.Name.Suffix()}-{r.Name.FullName()}") // Random full name
+            .RuleFor(u => u.Email, f => f.Internet.Email()) // Random email
+            .RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber()) // Random phone number
+            .RuleFor(u => u.DateOfBirth, f => f.Date.Past(30, DateTime.Now.AddYears(-18))) // Date of birth between 18 and 48 years ago
+            .RuleFor(u => u.Address, f => f.Address.FullAddress()); // Random address
+
+            List<User> fakeUsers = userFaker.Generate(10);
+
+            foreach (var user in fakeUsers)
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ BogusDataGeneration/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusDataGeneration
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(FullName)}: {FullName},{nameof(Email)}: {Email}," +
+                $" {nameof(PhoneNumber)}: {PhoneNumber}, {nameof(DateOfBirth)}: {DateOfBirth}, " +
+                $"{nameof(Address)}: {Address}";
+        }
+    }
+}
